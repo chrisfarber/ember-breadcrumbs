631437182f955c85628ca72646fc94d3e79cd55b
jevanlingen <jacobvanlingen@hotmail.com>
2015-10-28 15:14:15 +0100

    Fixes deprecation initializer for Ember 2.x
    DEPRECATION: The `initialize` method for Application initializer 'ember-breadcrumbs' should take only one argument - `App`, an instance of an `Application`. [deprecation id: ember-application.app-initializer-initialize-arguments] See http://emberjs.com/deprecations/v2.x/#toc_initializer-arity for more details.

--- app/initializers/bread-crumbs.js
+++ app/initializers/bread-crumbs.js
@@ -1,7 +1,13 @@
 export default {
   name: "ember-breadcrumbs",
-  initialize: function(container, app) {
-    app.inject("component:bread-crumbs", "router", "router:main");
-    app.inject("component:bread-crumbs", "applicationController", "controller:application");
+  initialize: function() {
+    //pre 2.x
+    if(arguments.length > 1) {
+      arguments[1].inject("component:bread-crumbs", "router", "router:main");
+      arguments[1].inject("component:bread-crumbs", "applicationController", "controller:application");  
+    } else {
+      arguments[0].inject("component:bread-crumbs", "router", "router:main");
+      arguments[0].inject("component:bread-crumbs", "applicationController", "controller:application");  
+    }
   }
 };
