c25d6182f85d90283cf28eaf6d892dbf2f235c31
jevanlingen <jacobvanlingen@hotmail.com>
2015-10-28 15:15:59 +0100

    Update bread-crumbs.js

--- app/initializers/bread-crumbs.js
+++ app/initializers/bread-crumbs.js
@@ -1,13 +1,8 @@
 export default {
   name: "ember-breadcrumbs",
   initialize: function() {
-    //pre 2.x
-    if(arguments.length > 1) {
-      arguments[1].inject("component:bread-crumbs", "router", "router:main");
-      arguments[1].inject("component:bread-crumbs", "applicationController", "controller:application");  
-    } else {
-      arguments[0].inject("component:bread-crumbs", "router", "router:main");
-      arguments[0].inject("component:bread-crumbs", "applicationController", "controller:application");  
-    }
+    let application = arguments[1] || arguments[0];
+    application.inject("component:bread-crumbs", "router", "router:main");
+    application.inject("component:bread-crumbs", "applicationController", "controller:application");
   }
 };
